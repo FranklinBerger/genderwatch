f2c86b9dbcaf3d7da93193e70d99a870d48aee2e
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-01-10 23:06:50 +0100

    index.php: reindent after removed html balise error

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ session_start();
 session_destroy();
 
 if (isset($_COOKIE["GW_user"]) and isset($_COOKIE["GW_password"])) {
-	// Cookie de loggin automatique exitsant, on part au loggin
-	header("Location:loggin.php");
+   // Cookie de loggin automatique exitsant, on part au loggin
+   header("Location:loggin.php");
 } else {
-	// Pas de loggin automatique, on fait rien
-	NULL;
+   // Pas de loggin automatique, on fait rien
+   NULL;
 }
 
 //Pour le bouton share
@@ -25,48 +25,48 @@ include("html_module.php");
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="icon" href="graphic/icone.png"/>
-	<title>GenderWatch</title>
-	<link rel="stylesheet" href="graphic/css.css"/>
+    <link rel="icon" href="graphic/icone.png"/>
+    <title>GenderWatch</title>
+    <link rel="stylesheet" href="graphic/css.css"/>
 </head>
 <body>
 
 <h1>Gender Watch</br>Protocole</h1>
 
-<image src="graphic/logo.png" align="middle" class="logo_index">
-	
-	</br>
-	</br>
-	</br>
-	<?php
-	if (isset($_GET["msg"])) {
-		echo "<p>" . $_GET["msg"] . "</p>";
-	} else {
-		echo "</br>";
-	}
-	?>
-	
-	<form action="loggin.php" method="post" class="singin_index_input">
-		<label for="index_input_user">Nom d'utilisat·eur·rice : </label>
-		</br>
-		<input type="text" name="index_input_user"
-		       class="singin_index_input"/>
-		</br>
-		</br>
-		<label for="index_input_pass">Mot de passe : </label>
-		</br>
-		<input type="password" name="index_input_pass"
-		       class="singin_index_input"/>
-		</br>
-		</br>
-		<button action="submit">Valider</button>
-	</form>
-	<?php bouton_lien("Voir un Watch partagé", "share"); ?>
-	
-	
-	
-	<?php
-	include("footer.php");
-	?>
+<img src="graphic/logo.png" align="middle" class="logo_index">
+
+</br>
+</br>
+</br>
+<?php
+if (isset($_GET["msg"])) {
+   echo "<p>" . $_GET["msg"] . "</p>";
+} else {
+   echo "</br>";
+}
+?>
+
+<form action="loggin.php" method="post" class="singin_index_input">
+    <label for="index_input_user">Nom d'utilisat·eur·rice : </label>
+    </br>
+    <input type="text" name="index_input_user"
+           class="singin_index_input"/>
+    </br>
+    </br>
+    <label for="index_input_pass">Mot de passe : </label>
+    </br>
+    <input type="password" name="index_input_pass"
+           class="singin_index_input"/>
+    </br>
+    </br>
+    <button action="submit">Valider</button>
+</form>
+<?php bouton_lien("Voir un Watch partagé", "share"); ?>
+
+
+
+<?php
+include("footer.php");
+?>
 </body>
 </html>
