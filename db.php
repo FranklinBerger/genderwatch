239a51e75e3f293a48fe83091103202da1c280dd
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-01-10 20:12:55 +0100

    db.php: extracted credentials in .const.php | created template .const.php.example
    git ignored the .const.php

--- db.php
+++ db.php
@@ -1,8 +1,9 @@
 <?php
 // Ouverture DB
 try{
+    require ".const.php";   //get credentials
 	$database = new PDO(
-	"mysql:host=localhost;dbname=gender_watch;charset=utf8", "root", "",
+	"mysql:host=$dbhost;dbname=$dbname;charset=utf8", $username, $password,
 	array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
 	die("Error à l'ouverture de la DB : " . $e->getMessage());
