3908b9066d7e2bc778354339d8cb6459f59eed7d
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-01-20 13:19:12 +0100

    index: login form fields required and separated 2 buttons and refactor

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ session_start();
 session_destroy();
 
 if (isset($_COOKIE["GW_user"]) and isset($_COOKIE["GW_password"])) {
-   // Cookie de loggin automatique exitsant, on part au loggin
-   header("Location:loggin.php");
+    // Cookie de loggin automatique exitsant, on part au loggin
+    header("Location:loggin.php");
 } else {
-   // Pas de loggin automatique, on fait rien
-   NULL;
+    // Pas de loggin automatique, on fait rien
+    NULL;
 }
 
 //Pour le bouton share
@@ -30,36 +30,37 @@ include("html_module.php");
     <link rel="stylesheet" href="graphic/css.css"/>
 </head>
 <body>
-<div class="appBody loginForm">
+<div class="appBody marginauto">
     <h1>Gender Watch</br>Protocole</h1>
 
     <div class="fullwidth flexdiv">
         <img src="graphic/logo.png" class="logo_index">
     </div>
-   <?php
-   if (isset($_GET["msg"])) {
-      echo "<p>" . htmlentities($_GET["msg"]) . "</p>";
-   } else {
-      echo "</br>";
-   }
-   ?>
+    <?php
+    if (isset($_GET["msg"])) {
+        echo "<p>" . htmlentities($_GET["msg"]) . "</p>";
+    } else {
+        echo "</br>";
+    }
+    ?>
 
-    <form action="loggin.php" method="post" class="loginForm">
-        <label for="index_input_user">Nom d'utilisat·eur·rice : </label>
+    <form action="loggin.php" method="post" class="bigmargintop marginauto displayblock">
+        <label for="index_input_user">Nom d'utilisateur·rice:</label>
         </br>
         <input type="text" name="index_input_user"
-               class="singin_index_input fullwidth"/>
+               class="singin_index_input widthmozavailable" required autofocus/>
         </br>
         </br>
-        <label for="index_input_pass">Mot de passe : </label>
+        <label for="index_input_pass">Mot de passe:</label>
         </br>
         <input type="password" name="index_input_pass"
-               class="singin_index_input fullwidth"/>
+               class="singin_index_input widthmozavailable" required/>
         </br>
         </br>
         <button>Connexion</button>
     </form>
-   <?php bouton_lien("Voir un Watch partagé", "share"); ?>
+    <br>
+    <div class="flexdiv box-alignright"><?php bouton_lien("Voir un Watch partagé", "share"); ?></div>
 </div>
 
 <?php
